2a5b931e7f826c377d0a05f518b73a1cd2c98d9a
kdanna <kayce.danna@gmail.com>
2016-08-09 16:14:22 -0600

    able to access values from facebook return hash. adding facebook_id to users schema

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -2,9 +2,17 @@ class Auth0Controller < ApplicationController
  def callback
     # This stores all the user information that came from Auth0
     # and the IdP
-    session[:userinfo] = request.env['omniauth.auth']
-
+    facebook_id = request.env['omniauth.auth']['uid'].split('|')[1]
+    ap facebook_id
+    first_name = request.env['omniauth.auth']['info']['first_name']
+    ap first_name
+    last_name = request.env['omniauth.auth']['info']['last_name']
+    ap last_name
+    user_image = request.env['omniauth.auth']['info']['image']
+    ap user_image
     # Redirect to the URL you want after successfull auth
+
+    #User.create ()
     redirect_to '/'
   end
 
