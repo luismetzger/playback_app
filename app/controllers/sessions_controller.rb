a049c88d5a165467bb28fc05b71cb02031ce111e
luismetzger <luismetzger@me.com>
2016-08-10 11:09:29 -0600

    fixed issues with logout button, controler clean up and delete route.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
 
-	def login(user)	
-		
-		@current_user = user
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to "/", :notice => "Logged Out"
 	end
 
+	private
 
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :profile_photo)
+	end
 
 
 end
