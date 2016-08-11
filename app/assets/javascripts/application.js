b3eb069cbcb796549f1a5b103df8ba257e74d7f5
luismetzger <luismetzger@me.com>
2016-08-10 21:48:55 -0600

    production updates.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require jquery.turbolinks
+//= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
 
